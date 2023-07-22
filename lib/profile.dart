a8966a534610e0fbf408e0cc73bcf65f07f1f8a7
Sita Ram Thing <sitaramthing667@gmail.com>
2023-07-22 17:35:18 +0545

    create the new circule shap

--- lib/profile.dart
+++ lib/profile.dart
@@ -8,11 +8,23 @@ class Profile extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
           child: Container(
-        alignment: Alignment.center,
-        child: const Column(children: [
-            Text("data") // txt
-        ]),
-      )),
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(15),
+              child: SizedBox(
+                  height: 150,
+                  width: 150,
+                  //square box; equal height and width so that it won't look like oval
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(80),
+                      //set border radius more than 50% of height and width to make circle
+                    ),
+                    color: Colors.lightBlue,
+                    elevation: 10,
+                  )
+              )
+          )
+        ),
     );
   }
 }
